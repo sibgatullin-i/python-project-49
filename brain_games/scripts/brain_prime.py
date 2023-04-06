e8a1796494e68d04f84669bbaf3c66c19da367ed
sibgatullin-i <67420207+sibgatullin-i@users.noreply.github.com>
2023-04-06 12:32:35 +0300

    brain-prime

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,20 +1,11 @@
-from random import randint
-
-DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
-MIN_NUMBER = 1
-MAX_NUMBER = 10
+#!/usr/bin/env python3
+from brain_games.game_logic import start_game
+from brain_games.games import prime
 
 
-def is_prime(num):
-    if num <= 1:
-        return False
-    divider = 2
-    while num % divider != 0:
-        divider += 1
-    return divider == num
+def main():
+    start_game(prime)
 
 
-def generate_round():
-    num = randint(MIN_NUMBER, MAX_NUMBER)
-    correct_answer = 'yes' if is_prime(num) else 'no'
-    return str(num), 
+if __name__ == '__main__':
+    main()
